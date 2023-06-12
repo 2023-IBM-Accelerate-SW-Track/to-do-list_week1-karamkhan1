1170f68cda82121346b724d8153c91c629928cd7
karamkhan1 <karamkhan0258@gmail.com>
2023-06-12 23:28:43 +0100

    terminal commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+    <div className="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src="../assets/pfp.png"
+          alt="Profile Pic"
+          width="200px"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Your Name</div>
+        <div className="brief_description">
+          // Details about you goes here
+        </div>
+      </div>
+    </div>
+  </div>
       </div>
     )
   }
